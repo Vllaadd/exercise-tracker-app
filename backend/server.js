01893d77e6e26d611b49d872157e2d11c2813873
Vllaadd <vlladdiimiir@gmail.com>
2022-10-24 01:49:54 -0400

    connection to mongodb established

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+
 //•••NEVIRONMENTAL VARIABLES•••
 require("dotenv").config(); //this configures so we can have environment variables 
 
 const app = express();
 const port = process.env.PORT || 3000;
+
 //•••MIDDLEWARE•••
 app.use(cors()); //•••cors middleware•••
 app.use(express.json()); //••this will us to parse json••• 
+
+//•••MONGODB DATABASE•••
+const uri = process.env.ATLAS_URI; //uri is where our database is stored
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDB database connection established successfully");
+});
 
 //•••STARTING THE SERVER, LISTENING THE PORT•••
 app.listen(port, () => {
