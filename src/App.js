e86076318c40cd7778b72803438462d2d34932fb
Vllaadd <vlladdiimiir@gmail.com>
2022-10-29 05:30:45 -0400

    navbar showed up still a few remaining issues

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-
+import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import ExercisesList from "./components/ExercisesList";
@@ -12,16 +10,15 @@ import CreateUser from "./components/CreateUser";
 
 function App() {
   return (
-      <Router>
-        <div className="container">
-          <Navbar />
-          <br />
-          <Route path="/" exact component={ExercisesList} />
+<div className="container">
+    <Navbar />
+    <Routes>
+          <Route path="/" component={ExercisesList} />
           <Route path="edit/:id" component={EditExercise} />
           <Route path="create" component={CreateExercise} />
           <Route path="/user" component={CreateUser} />
-        </div>
-      </Router>
+    </Routes>
+    </div>
   );
 }
 
